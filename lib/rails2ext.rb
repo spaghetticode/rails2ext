5db5ee9fe11594182e8cea09d98c5665f6050744
andrea longhi <andrea@spaghetticode.it>
2012-07-11 11:54:36 +0200

    require lib files with explicit path

--- lib/rails2ext.rb
+++ lib/rails2ext.rb
@@ -1,6 +1,7 @@
 module Rails2ext
   require 'active_support/all'
   require 'active_record'
-  require 'rails2ext/active_support'
-  require 'rails2ext/active_record'
+  %w[active_support active_record].each do |filename|
+    require File.expand_path "../rails2ext/#{filename}", __FILE__
+  end
 end
